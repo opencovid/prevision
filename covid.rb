71590cd5cf7d4576afa70e1e0d258e5b76c9d331
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-03-25 09:26:23 +0100

    english version

--- covid.rb
+++ covid.rb
@@ -1,18 +1,20 @@
+require 'date'
+
 deaths = 1
 rest = 0
 infected = deaths * 50
 r0 = 2.7
 population = 7_500_000_000
-day = 1
+day = Date.new(2020,1,11)
 
-# Optimistic (based on diamond | total around 5000 | infected 712 | deaths 10)
+# Diamond princess | total around 5000 | infected 712 | deaths 10)
 max_infected = (0.14*population).round
 
 # Prevision
 # max_infected = ((1-(1/r0))*population).round
 
 loop do
-  sleep 1
+  sleep 0.5
   return if infected > max_infected 
 
   additional_deaths = deaths * 0.15
@@ -28,7 +30,7 @@ loop do
 
   infected = deaths * 50
 
-  puts "Day #{day} | Infected #{infected} | Deaths #{deaths}"
-  day += 1
+  puts "#{day.strftime("%d/%m/%Y")} | Infected #{infected} | Deaths #{deaths}"
+  day = day + 1
 end
 
